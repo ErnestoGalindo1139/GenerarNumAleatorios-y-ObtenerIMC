edc47e2399a95aa56ab70aa493fe98d21704abe3
Ernesto David Galindo Hernández <galindohernandezernesto@gmail.com>
2022-11-09 12:11:12 -0700

    Boton generar terminado

--- js/app.js
+++ js/app.js
@@ -1,8 +1,21 @@
 function generar(){
-    var edad = document.getElementById('edad');
+    let edad = document.getElementById('edad');
+    let altura= document.getElementById('altura');
+    let peso= document.getElementById('peso');
 
-        let aleatorio = Math.floor(Math.random()*(99)+1); 
+    let aleatorioEdad = Math.floor(Math.random()*(99)+1); 
+
+    edad.value = aleatorioEdad;
     
-        edad.value = aleatorio;
+    let aleatorioAltura = (Math.random()*(2.5 - 1.5) + 1.5).toFixed(2);
     
+    altura.value = aleatorioAltura;
+
+    let aleatorioPeso = (Math.random()*(130 - 20) + 20).toFixed(2);
+
+    peso.value = aleatorioPeso;
+
 }
+
+
+
