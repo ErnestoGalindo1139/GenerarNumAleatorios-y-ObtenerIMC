3d5fff7df5b32adbb213660de2636c546263a94d
Ernesto David Galindo Hernández <galindohernandezernesto@gmail.com>
2022-11-09 13:11:31 -0700

    Boton borrar registros terminado

--- js/app.js
+++ js/app.js
@@ -28,14 +28,45 @@ function calcular() {
     document.getElementById('imc').value = imc.toFixed(2);
 
     if(imc < 18.5){
-        nivel.value = "Bajo peso"
+        nivel.value = "Bajo peso";
     }else if(imc >= 18.5 && imc <= 24.9){
-        nivel.value = "Peso Saludable"
+        nivel.value = "Peso Saludable";
     }else if(imc >= 25 && imc <= 29.9){
-        nivel.value = "Sobrepeso"
+        nivel.value = "Sobrepeso";
     }else if(imc >= 30) {
-        nivel.value = "Obesidad"
+        nivel.value = "Obesidad";
     }
 
 }
+
+function registrar(){
+    let edad = document.getElementById('edad').value;
+    let altura= document.getElementById('altura').value;
+    let peso = document.getElementById('peso').value;
+    let imc = document.getElementById('imc').value
+    let nivel = document.getElementById('nivel').value;
+
+    let registros = document.getElementById('registros');
+
+    if(edad == ""){
+        alert('Presione el boton de Generar');
+
+    }else if(imc == ""){
+        alert('Presione el boton de Calcular');
+    }else{
+        registros.innerHTML += "<p>" + " Edad: " +  edad + " " + " Altura: " + altura + " " + " Peso: "+ peso + " " + " IMC: "+ imc + " " + " Nivel: " + nivel + "</p>"
+    }
+
+
+}
+
+function borrar(){
+    registros.innerHTML = "";
+    document.getElementById("imc").value = "";
+    document.getElementById("nivel").value = "";
+    document.getElementById("edad").value = "";
+    document.getElementById("altura").value = "";
+    document.getElementById("peso").value = "";
+}
+
 
