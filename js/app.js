9fa3d5743e35f16911826163005e6a4391f29331
Ernesto David Galindo Hernández <galindohernandezernesto@gmail.com>
2022-11-09 12:28:06 -0700

    Boton calcular terminado

--- js/app.js
+++ js/app.js
@@ -17,5 +17,25 @@ function generar(){
 
 }
 
+function calcular() {
+    let edad = document.getElementById('edad').value;
+    let altura= document.getElementById('altura').value;
+    let peso = document.getElementById('peso').value;
+    let nivel = document.getElementById('nivel');
 
+    let imc = (peso / Math.pow(altura, 2));
+    
+    document.getElementById('imc').value = imc.toFixed(2);
+
+    if(imc < 18.5){
+        nivel.value = "Bajo peso"
+    }else if(imc >= 18.5 && imc <= 24.9){
+        nivel.value = "Peso Saludable"
+    }else if(imc >= 25 && imc <= 29.9){
+        nivel.value = "Sobrepeso"
+    }else if(imc >= 30) {
+        nivel.value = "Obesidad"
+    }
+
+}
 
